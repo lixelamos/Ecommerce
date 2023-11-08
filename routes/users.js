78d995302e88ee9064f7d5ba8fb9d18bfa09a1d4
Amos <amosbett99@gmail.com>
2023-11-08 16:28:55 +0300

    token verification

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,25 @@
-const router=require("express").Router()
+const {verifyToken, verifyTokenAndAuthorization} =require("./verifyToken.js")
+
+const router=require("express").Router();
+
+
+//UPDATE 
+
+router.put("/:id",verifyTokenAndAuthorization,async (req,res)=>{
+if (req.body.password){
+    req.body.password=CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SECRET).toString();
+
+}
+try{
+    const updatedUser= await User.findByIdAndUpdate(req.params.id,{
+        $set:req.body,
+    },{new:true})
+    res.status(200).json("updated user")
+}catch(err){
+        res.status(500).json(err);
+    }
+    res.status(200).json(updatedUser)
+});
 
 
 
